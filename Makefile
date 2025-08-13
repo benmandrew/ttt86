8e52ce6c24bbbe1e56592f371b7146112213c72d
benmandrew <benmandrew@gmail.com>
2025-08-13 15:02:48 +0100

    Extracted to multiple assembly files

--- Makefile
+++ Makefile
@@ -1,19 +1,23 @@
 .PHONY: all clean
 
 ASMFLAGS = -f elf64 -W+all
-LINKFLAGS =
+LINKFLAGS = -static
+SRC_DIR = src
 BUILD_DIR = build
+
+SOURCES = $(wildcard $(SRC_DIR)/*.s)
+OBJECTS = $(patsubst $(SRC_DIR)/%.s,$(BUILD_DIR)/%.o,$(SOURCES))
 
 all: $(BUILD_DIR)/main
 
-$(BUILD_DIR)/main.o: main.s | $(BUILD_DIR)
-	nasm $(ASMFLAGS) -o $@ main.s
+clean:
+	rm -rf $(BUILD_DIR)
 
-$(BUILD_DIR)/main: $(BUILD_DIR)/main.o | $(BUILD_DIR)
-	ld $(LINKFLAGS) $< -o $(BUILD_DIR)/main
+$(BUILD_DIR)/%.o: $(SRC_DIR)/%.s | $(BUILD_DIR)
+	nasm $(ASMFLAGS) -o $@ $<
+
+$(BUILD_DIR)/main: $(OBJECTS) | $(BUILD_DIR)
+	ld $(LINKFLAGS) $(OBJECTS) -o $@
 
 $(BUILD_DIR):
 	mkdir -p $(BUILD_DIR)
-
-clean:
-	rm -rf $(BUILD_DIR)
--- src/main.s
+++ src/main.s
@@ -7,34 +7,11 @@ section .data
 
 section .bss
     input_char resb 1
-    termios_raw resb 32   ; struct termios is 32 bytes on x86-64
 
 section .text
     global _start
 
-; Terminal raw mode for immediate user input without waiting for enter
-set_raw_mode:
-    ; TCGETS = 0x5401, TCSETS = 0x5402
-    mov rax, 16           ; syscall: ioctl
-    mov rdi, 0            ; fd = stdin
-    mov rsi, 0x5401       ; TCGETS
-    mov rdx, termios_raw
-    syscall
-
-    ; Modify termios_raw: unset ICANON (0x0002) and ECHO (0x0008) in c_lflag
-    ; c_lflag is at offset 12 in termios struct
-    mov rbx, termios_raw
-    add rbx, 12
-    mov eax, [rbx]
-    and eax, 0xFFFFFFFFFFFFFFF5  ; clear ICANON (bit 1) and ECHO (bit 3)
-    mov [rbx], eax
-
-    mov rax, 16           ; syscall: ioctl
-    mov rdi, 0            ; fd = stdin
-    mov rsi, 0x5402       ; TCSETS
-    mov rdx, termios_raw
-    syscall
-    ret
+extern set_raw_mode
 
 _start:
     call set_raw_mode
@@ -65,6 +42,7 @@ get_input:
     ; Check upper bound
     cmp    rax, 9
     jg     invalid_input
+    push   rax
     jmp    exit
 
 invalid_input:
@@ -82,9 +60,8 @@ exit:
     mov rdi, 1              ; fd = stdout
     mov rsi, newline
     mov rdx, 1
-    syscall
 
     ; exit(number)
-    mov rdi, rax            ; exit code
+    pop rdi                 ; exit code
     mov rax, 60             ; syscall: exit
     syscall
